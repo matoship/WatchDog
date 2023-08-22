de5cf0459279a99f64b8f59a26a581e3a8f41945
anupama4you <43500022+anupama4you@users.noreply.github.com>
2023-08-23 01:24:49 +0930

    caregiver edit profile UI

--- A1840282_Anupama/lib/screens/caregiver_profile.dart
+++ A1840282_Anupama/lib/screens/caregiver_profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:watchdog_correct/reusable_widgets/text_box.dart';
+import 'package:watchdog_correct/screens/home_screen.dart';
 
 import '../utils/color_utils.dart';
 
@@ -16,98 +17,158 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   // Editing state
   bool isEditing = false;
+  bool isObscurePassword = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Profile')),
+      appBar: AppBar(
+        title: Text('Profile'),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomeScreen()),
+              );
+            },
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            )),
+        actions: [
+          IconButton(
+              onPressed: (){},
+              icon: const Icon(
+                Icons.settings,
+                color: Colors.black,
+              ))
+        ]),
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(gradient: backgroundGradient()),
-        child: Column(
-          children: [
-            CircleAvatar(
-              backgroundImage: NetworkImage('https://image.shutterstock.com/image-photo/closeup-portrait-old-man-260nw-62671255.jpg'),
-            ),
-            isEditing
-                ? TextField(
-              decoration: InputDecoration(labelText: 'Full Name'),
-              onChanged: (value) {
-                setState(() {
-                  fullName = value;
-                });
-              },
-            )
-                : MyTextBox(text: fullName, sectionName: fullName),
-            isEditing
-                ? TextField(
-              decoration: InputDecoration(labelText: 'Phone Number'),
-              onChanged: (value) {
-                setState(() {
-                  phoneNumber = value;
-                });
-              },
-            )
-                : Text(phoneNumber),
-            isEditing
-                ? TextField(
-              decoration: InputDecoration(labelText: 'Address'),
-              onChanged: (value) {
-                setState(() {
-                  address = value;
-                });
-              },
-            )
-                : Text(address),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      if (isEditing) {
-                        // Save changes
-                      }
-                      isEditing = !isEditing;
-                    });
-                  },
-                  child: Text(isEditing ? 'Save' : 'Edit'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    // Show confirmation dialog for account removal
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          title: Text('Remove Account'),
-                          content: Text(
-                              'Are you sure you want to remove your account?'),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                // Perform account removal
-                                Navigator.pop(context);
-                              },
-                              child: Text('Yes'),
-                            ),
-                            TextButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              child: Text('No'),
-                            ),
+        padding: EdgeInsets.only(left: 15, top: 20, right: 15),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: ListView(
+            children: [
+              Center(
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 130,
+                      height: 130,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 4, color: Colors.white),
+                          boxShadow: [
+                            BoxShadow(
+                                spreadRadius: 2,
+                                blurRadius: 10,
+                                color: Colors.black.withOpacity(0.1)
+                            )
                           ],
-                        );
-                      },
-                    );
-                  },
-                  child: Text('Remove Account'),
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLhi4qVtieeHmMjiglFczNiG1ijmVPa6BwGA&usqp=CAU'
+                              )
+                          )
+                      ),
+                    ),
+                    Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                  width: 4,
+                                  color: Colors.white
+                              ),
+                              color: Colors.blue
+                          ),
+                          child: Icon(
+                            Icons.edit,
+                            color: Colors.white,
+                          ),
+                        )
+                    )
+                  ],
                 ),
-              ],
-            ),
-          ],
+              ),
+              SizedBox(height: 30),
+              buildTextField("Full Name", "Demon", false),
+              buildTextField("Email", "anupama.dilshan@icloud.com", false),
+              buildTextField("Password", "******", true),
+              buildTextField("Address", "Adelaide CDB", false),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  OutlinedButton(
+                      onPressed: () {},
+                      child: const Text("CANCEL",
+                        style: TextStyle(
+                          fontSize: 15,
+                          letterSpacing: 2,
+                          color: Colors.white
+                        )),
+                    style: OutlinedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(horizontal: 50),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {},
+                      child: Text("SAVE", style: TextStyle(
+                        fontSize: 15,
+                        letterSpacing: 2,
+                        color: Colors.white
+                      )),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        padding: EdgeInsets.symmetric(horizontal: 50),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+
+      ),
+    );
+  }
+
+  Widget buildTextField(String labelText, String placeholder, bool isPasswordTextField){
+    return Padding(
+      padding: EdgeInsets.only(bottom: 30),
+      child: TextField(
+        obscureText: isPasswordTextField ? isObscurePassword : false,
+        decoration: InputDecoration(
+          suffixIcon: isPasswordTextField ?
+          IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.remove_red_eye, color: Colors.white)
+          ): null,
+          contentPadding: EdgeInsets.only(bottom: 5),
+          labelText: labelText,
+          labelStyle: TextStyle(
+            color: Colors.white70
+          ),
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          hintText: placeholder,
+          hintStyle: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.white
+          ),
         ),
       ),
     );
