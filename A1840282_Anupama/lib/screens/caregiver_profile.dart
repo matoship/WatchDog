0663ef67aedfbef2840c9e793568d8793cd6ad21
anupama4you <43500022+anupama4you@users.noreply.github.com>
2023-08-24 15:12:59 +0930

    caregiver profile update

--- A1840282_Anupama/lib/screens/caregiver_profile.dart
+++ A1840282_Anupama/lib/screens/caregiver_profile.dart
@@ -1,12 +1,22 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:watchdog_correct/reusable_widgets/text_box.dart';
 import 'package:watchdog_correct/screens/home_screen.dart';
 
 import '../utils/color_utils.dart';
 
+class Patient {
+  final String name;
+
+  Patient({required this.name});
+}
+
 class ProfileScreen extends StatefulWidget {
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
+
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
@@ -19,30 +29,49 @@ class _ProfileScreenState extends State<ProfileScreen> {
   bool isEditing = false;
   bool isObscurePassword = true;
 
+  PickedFile ? _imageFile;
+  final ImagePicker _picker = ImagePicker();
+  List<Patient> assignedPatients = [
+    Patient(name: 'Kaifeng'),
+    Patient(name: 'ooom'),
+    Patient(name: 'Revaan'),
+    // Add more patients as needed
+  ];
+
+  void takePhoto(ImageSource source) async {
+    final pickedFile = await _picker.pickImage(
+      source: source,
+    );
+    setState(() {
+      _imageFile = pickedFile as PickedFile;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
-        leading: IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomeScreen()),
-              );
-            },
-            icon: Icon(
-              Icons.arrow_back,
-              color: Colors.black,
-            )),
-        actions: [
-          IconButton(
-              onPressed: (){},
-              icon: const Icon(
-                Icons.settings,
+          title: Text('Profile'),
+          leading: IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomeScreen()),
+                );
+              },
+              icon: Icon(
+                Icons.arrow_back,
                 color: Colors.black,
-              ))
-        ]),
+              )),
+          actions: [
+            IconButton(
+                onPressed: (){},
+                icon: const Icon(
+                  Icons.settings,
+                  color: Colors.black,
+                ))
+          ]),
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
@@ -72,9 +101,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
                           shape: BoxShape.circle,
                           image: DecorationImage(
                               fit: BoxFit.cover,
-                              image: NetworkImage(
-                                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLhi4qVtieeHmMjiglFczNiG1ijmVPa6BwGA&usqp=CAU'
-                              )
+                            image: _imageFile == null
+                                ? AssetImage("assets/images/albert_dp.avif")
+                                : AssetImage("assets/images/albert_dp.avif"),
                           )
                       ),
                     ),
@@ -92,9 +121,17 @@ class _ProfileScreenState extends State<ProfileScreen> {
                               ),
                               color: Colors.blue
                           ),
-                          child: Icon(
-                            Icons.edit,
-                            color: Colors.white,
+                          child: InkWell(
+                            onTap: () {
+                              showModalBottomSheet(
+                                  context: context,
+                                  builder: ((builder) => bottomSheet())
+                              );
+                            },
+                            child: Icon(
+                              Icons.edit,
+                              color: Colors.white,
+                            ),
                           ),
                         )
                     )
@@ -102,34 +139,45 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 ),
               ),
               SizedBox(height: 30),
-              buildTextField("Full Name", "Demon", false),
+              buildTextField("Full Name", "Anupama Dilshan", false),
               buildTextField("Email", "anupama.dilshan@icloud.com", false),
               buildTextField("Password", "******", true),
+              buildTextField("Phone No", "0452575523", false),
               buildTextField("Address", "Adelaide CDB", false),
+              buildTextField("Assigned Patients", assignedPatients.map((patient) => patient.name).join(', '), false),
+              // Add the ListView.builder for the assigned patients
+              // ListView.builder(
+              //   shrinkWrap: true, // To allow the ListView to take the necessary height
+              //   itemCount: assignedPatients.length,
+              //   itemBuilder: (context, index) {
+              //     final patient = assignedPatients[index];
+              //     return buildTextField("Address", patient.name, false);
+              //   },
+              // ),
               SizedBox(height: 30),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   OutlinedButton(
-                      onPressed: () {},
-                      child: const Text("CANCEL",
+                    onPressed: () {},
+                    child: const Text("CANCEL",
                         style: TextStyle(
-                          fontSize: 15,
-                          letterSpacing: 2,
-                          color: Colors.white
+                            fontSize: 15,
+                            letterSpacing: 2,
+                            color: Colors.white
                         )),
                     style: OutlinedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(horizontal: 50),
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+                        padding: EdgeInsets.symmetric(horizontal: 50),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
                     ),
                   ),
                   ElevatedButton(
-                      onPressed: () {},
-                      child: Text("SAVE", style: TextStyle(
+                    onPressed: () {},
+                    child: Text("SAVE", style: TextStyle(
                         fontSize: 15,
                         letterSpacing: 2,
                         color: Colors.white
-                      )),
+                    )),
                     style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.blue,
                         padding: EdgeInsets.symmetric(horizontal: 50),
@@ -146,7 +194,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
+
   Widget buildTextField(String labelText, String placeholder, bool isPasswordTextField){
+
     return Padding(
       padding: EdgeInsets.only(bottom: 30),
       child: TextField(
@@ -160,7 +210,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
           contentPadding: EdgeInsets.only(bottom: 5),
           labelText: labelText,
           labelStyle: TextStyle(
-            color: Colors.white70
+              color: Colors.white70
           ),
           floatingLabelBehavior: FloatingLabelBehavior.always,
           hintText: placeholder,
@@ -173,4 +223,45 @@ class _ProfileScreenState extends State<ProfileScreen> {
       ),
     );
   }
+
+  Widget bottomSheet() {
+    return Container(
+      height: 100.0,
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 20,
+      ),
+      child: Column(
+        children: <Widget>[
+          Text(
+            "Choose Profile photo",
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            ElevatedButton.icon(
+              icon: Icon(Icons.camera),
+              onPressed: () {
+                takePhoto(ImageSource.camera);
+              },
+              label: Text("Camera"),
+            ),
+            ElevatedButton.icon(
+              icon: Icon(Icons.image),
+              onPressed: () {
+                takePhoto(ImageSource.gallery);
+              },
+              label: Text("Gallery"),
+            ),
+          ])
+        ],
+      ),
+    );
+  }
+
 }
