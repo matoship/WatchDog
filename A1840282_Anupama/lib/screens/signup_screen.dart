9f1075e3e56bc6a22010da2e2ebce017d21e5a30
anupama4you <43500022+anupama4you@users.noreply.github.com>
2023-08-31 00:29:34 +0930

    add caregiver - post done

--- A1840282_Anupama/lib/screens/signup_screen.dart
+++ A1840282_Anupama/lib/screens/signup_screen.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:watchdog_correct/reusable_widgets/reusable_widget.dart';
 import 'package:watchdog_correct/utils/color_utils.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import 'home_screen.dart';
 
@@ -56,19 +58,77 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     const SizedBox(
                       height: 20,
                     ),
-                    firebaseUIButton(context, "Sign up", () {
-                      FirebaseAuth.instance
-                          .createUserWithEmailAndPassword(
-                          email: _emailTextController.text,
-                          password: _passwordTextController.text)
-                          .then((value) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Created New Account')));
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => HomeScreen()));
-                      }).onError((error, stackTrace) {
-                        print("Error ${error.toString()}");
-                      });
+                    firebaseUIButton(context, "Sign up", () async {
+                      try {
+                        await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                            email: _emailTextController.text,
+                            password: _passwordTextController.text)
+                            .then((value) async {
+
+                          // Create a map with the user data to send to the API
+                          final Map<String, dynamic> userData = {
+                            'username': _userNameTextController.text,
+                            'email': _emailTextController.text,
+                            'firstName': '',
+                            'lastName': '',
+                            'phone': '',
+                            'assignedPatients': []
+                            // Add other user data here
+                          };
+
+                          // Encode the userData map to JSON and exclude undefined values
+                          final jsonData = json.encode(userData, toEncodable: (dynamic value) {
+                            if (value == null) {
+                              return null;
+                            }
+                            return value;
+                          });
+
+                          final response = await http.post(
+                            Uri.parse('https://us-central1-watchdog-gamma.cloudfunctions.net/app/caregivers'), // Replace with your API endpoint URL
+                            headers: {'Content-Type': 'application/json'},
+                            body: jsonData,
+                          );
+
+                          print(response.body);
+                          print(jsonData);
+
+                          if (response.statusCode == 200) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(content: Text('Created New Account')));
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => HomeScreen()),
+                            );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(content: Text('Failed to add user')));
+                          }
+                        });
+                      } catch (error) {
+                        // Handle different error scenarios
+                        if (error is FirebaseAuthException) {
+                          String errorMessage = '';
+
+                          if (error.code == 'email-already-in-use') {
+                            errorMessage = 'The email address is already in use by another account.';
+                          } else if (error.code == 'weak-password') {
+                            errorMessage = 'The password is too weak.';
+                          } else if (error.code == 'invalid-email') {
+                            errorMessage = 'Invalid email address.';
+                          } else {
+                            errorMessage = 'An error occurred: ${error.message}';
+                          }
+
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text(errorMessage)),
+                          );
+                        } else {
+                          // Fallback for any other errors
+                          print("Error: $error");
+                        }
+                      }
                     })
                   ],
                 ),
