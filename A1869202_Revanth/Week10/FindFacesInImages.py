f09b57808e643b0c5d5d2cf8a300042c734ac72f
chenkaifeng <chenkaifeng@jianke.com>
2023-10-13 16:56:37 +1030

    week10

--- A1869202_Revanth/Week10/FindFacesInImages.py
+++ A1869202_Revanth/Week10/FindFacesInImages.py
@@ -5,13 +5,8 @@
 
 import cv2
 import mediapipe as mp
-import time
-import imutils
-import argparse
 import os
 import sys
-import pickle
-import face_recognition
 from imutils import paths
 from pathlib import Path
 import collections
@@ -21,13 +16,15 @@ try:
 except:
     pass
 
+
 class FaceDetector():
     def __init__(self, minDetectionCon=0.5):
 
         self.minDetectionCon = minDetectionCon
 
         self.mpFaceDetection = mp.solutions.face_detection
-        self.faceDetection = self.mpFaceDetection.FaceDetection(self.minDetectionCon)
+        self.faceDetection = self.mpFaceDetection.FaceDetection(
+            self.minDetectionCon)
 
     def findFaces(self, img, draw=True):
         # returns faces - bounding boxes
@@ -39,9 +36,10 @@ class FaceDetector():
                 bboxC = detection.location_data.relative_bounding_box
                 ih, iw, ic = img.shape
                 bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
-                       int(bboxC.width * iw), int(bboxC.height * ih)
+                    int(bboxC.width * iw), int(bboxC.height * ih)
                 bboxs.append([id, bbox, detection.score])
         return bboxs
+
 
 def print_progress_bar(name, iteration, total, bar_length=50):
     if iteration == 1:
@@ -50,11 +48,12 @@ def print_progress_bar(name, iteration, total, bar_length=50):
     arrow = '-' * int(round(progress * bar_length) - 1) + '>'
     spaces = ' ' * (bar_length - len(arrow))
     name = name + "'s face:"
-    sys.stdout.write(f'\r[INFO] Encoding: {name} {arrow + spaces} {iteration}/{total}')
+    sys.stdout.write(
+        f'\r[INFO] Encoding: {name} {arrow + spaces} {iteration}/{total}')
     sys.stdout.flush()
 
-def FindFacesInImages(input_folder:Path):
 
+def FindFacesInImages(input_folder: Path):
     """
     This function converts Patients full images and detects the faces and saves them in the same folder, under the same name of the image file previously saved.
 
@@ -69,12 +68,12 @@ def FindFacesInImages(input_folder:Path):
     didntFindFaces = 0
     totalImages = len(imagePaths)
 
-    # loop over the image paths 
+    # loop over the image paths
     for (i, imagePath) in enumerate(imagePaths):
         # extract the person name from the image path
         name = imagePath.split(os.path.sep)[-2]
         print_progress_bar(name, i + 1, len(imagePaths))
-        
+
         # load the input image and detect faces in it and find bounding boxes
         img = cv2.imread(imagePath)
         bboxs = detector.findFaces(img)
@@ -85,21 +84,22 @@ def FindFacesInImages(input_folder:Path):
                 for i in range(len(bboxs)):
                     (startX, startY, w, h) = bboxs[i][1]
                     face = img[startY:startY+h, startX:startX+w]
-                    
+
                     # Save it with the same in imagePath
                     cv2.imwrite(imagePath, face)
 
             except:
                 # Print the error
                 # print("\n[ERROR] Error in finding bounding box in: ", imagePath)
-                
+
                 # if face is not found, delete the image
-                os.remove(imagePath)    
+                os.remove(imagePath)
         else:
-            didntFindFaces += 1 # "No face found in image"
-                
+            didntFindFaces += 1  # "No face found in image"
+
             # if face is not found, delete the image
             os.remove(imagePath)
 
-    print(f"\n[INFO] Model has an accuracy of: {((totalImages-didntFindFaces)/totalImages)*100:.1f}%, found faces in {(totalImages-didntFindFaces)} images, out of {totalImages}.")
+    print(
+        f"\n[INFO] Model has an accuracy of: {((totalImages-didntFindFaces)/totalImages)*100:.1f}%, found faces in {(totalImages-didntFindFaces)} images, out of {totalImages}.")
     print("[INFO] Found Faces In Images: Processing completed.")
